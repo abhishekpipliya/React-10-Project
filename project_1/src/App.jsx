947f33c651569b002fe435efcdc73bb5ad2de023
aadeepipliya <aadeepipliya@gmail.com>
2024-04-01 23:23:49 +0530

    mon 23 23 pm

--- project_1/src/App.jsx
+++ project_1/src/App.jsx
@@ -36,11 +36,13 @@ export default App
 */
 
 import "./App.css";
+import Herosection from "./components/Hero";
 import Navigation from "./components/navigation";
 
 const App = () => {
 return( <div>
  <Navigation/>
+ <Herosection/>
 </div>
 )};
 
