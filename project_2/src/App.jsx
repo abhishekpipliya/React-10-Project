26a87a9e08984fd890fcf71b61df77255549a3c9
aadeepipliya <aadeepipliya@gmail.com>
2024-04-04 00:08:04 +0530

    thursday 00 07 am

--- project_2/src/App.jsx
+++ project_2/src/App.jsx
@@ -1,7 +1,28 @@
 import "./App.css";
+import ContactHeader from "./components/ContactHeader/ContactHeader";
+import Navigation from "./components/Navigation/navigation";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navigation />
+      <ContactHeader />
+    </div>
+  );
 }
+export default App;
+
+/*import "./App.css";
+
+import Navigation from "./components/navigation";
+
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
+};
 
 export default App;
+*/
